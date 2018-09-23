d2c2a8f60f72747ba51b4798c82e17ce5d2a1230
Garrett Johnson <garrettrayj@gmail.com>
2018-09-23 00:29:07 -0600

    Initial Commit

---
+++ AutoReload/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  AutoReload
+//
+//  Created by Garrett Johnson on 9/23/18.
+//  Copyright © 2018 DevSci. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override var representedObject: Any? {
+        didSet {
+        // Update the view, if already loaded.
+        }
+    }
+
+
+}
+
